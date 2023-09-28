44b883552244ab26382a74cc45b57b597093360b
factism001 <factism001@gmail.com>
2023-09-28 22:18:25 +0100

    updated views,urls,forms

--- soilsite/soilapp/views.py
+++ soilsite/soilapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import SoilData, Profile
 from .forms import SignUpForm
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 
@@ -21,9 +24,33 @@ def register(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
+            auth_login(request, user)
             messages.success(request, 'Registration successful.')
             return redirect('index')
         messages.error(request, 'Unsuccessful registration. Invalid information.')
     form = SignUpForm()
     return render(request, 'registration/register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('index')
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'login_form': form})
+
+def logout_view(request):
+    auth_logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('index')
