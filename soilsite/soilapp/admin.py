c7edd6bdc6a27885deb26291770e5f24ff111505
factism001 <factism001@gmail.com>
2023-10-11 13:43:00 +0100

    debbuging

--- soilsite/soilapp/admin.py
+++ soilsite/soilapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import SoilData, Profile
+from .models import SoilProperty, Profile
 
 # Register your models here.
 
-admin.site.register(SoilData)
+admin.site.register(SoilProperty)
 admin.site.register(Profile)
--- soilsite/soilapp/forms.py
+++ soilsite/soilapp/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import SoilData
+from .models import SoilProperty
+from django.forms import formset_factory
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
@@ -17,18 +18,29 @@ class SignUpForm(UserCreationForm):
             user.save()
         return user
 
+'''
 class SoilDataForm(forms.ModelForm):
     class Meta:
         model = SoilData
         fields = ('soil_type', 'ph_level', 'nutrient_content')
+'''
 
 
+'''
 class SoilPropertiesForm(forms.Form):
     particle_size = forms.CharField(label='Particle Size', required=True)
     soil_ph = forms.CharField(label='Soil pH', required=True)
     cec = forms.CharField(label='Cation Exchange Capacity (CEC)', required=True)
     calcium = forms.CharField(label='Calcium', required=True)
     magnesium = forms.CharField(label='Magnesium', required=True)
+'''
+
+
+class SoilPropertiesForm(forms.Form):
+    name = forms.CharField(label='Property', max_length=100)
+    value = forms.CharField(label='Value', max_length=100)
+
+SoilPropertiesFormSet = formset_factory(SoilPropertiesForm, extra=1)
 
 
 class ChatForm(forms.Form):
